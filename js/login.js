7bad734f43045edf58c24cd62b963f9610ac567d
Md Nazmul Hossain Emon <imon7810@gmail.com>
2022-08-24 11:56:02 +0600

    added

--- js/login.js
+++ js/login.js
@@ -8,7 +8,7 @@ document.getElementById('submit-button').addEventListener('click', function () {
     // get user password :
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
-    
+
     // check email and password :
     if (userEmail == 'imonhossin104@gmail.com' && userPassword == 'secret') {
         window.location.href = 'banking.html';
